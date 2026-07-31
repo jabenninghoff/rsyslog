01e5313baf8d6eb90a3572e3f92963147e751a89
John Benninghoff <jbenninghoff@mac.com>
2026-07-31 13:34:21 -0500

    fix: implement copilot suggested fixes

--- tests/startup.sh
+++ tests/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # check rsyslog startup and version output
 # requires: docker-build.sh
+set -e
 
-docker run --rm --hostname rsyslog.test rsyslog:dev sh
+docker run --rm --hostname rsyslog.test rsyslog:dev rsyslogd -N1
